577eb1e7d7b9d90a7f21ae1e813c869828c89db0
Deepti <deeptic076@gmail.com>
2023-09-18 19:40:21 +0530

    updated hamburger menu

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom';
 
 
 const Navbar = () => {
+  // State to track the visibility of the mobile menu
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  // Function to toggle the mobile menu
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+  
   return (
     <>
         <div className="bg-white lg:pb-12">
@@ -27,13 +35,34 @@ const Navbar = () => {
             <Link to="/contact" className="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base">Contact</Link>
           </div>
 
-          <button type="button" className="inline-flex items-center gap-2 rounded-lg bg-gray-200 px-2.5 py-2 text-sm font-semibold text-gray-500 ring-indigo-300 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
-              <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
-            </svg>
+          {/* Mobile Menu Button */}
+          <button
+              type="button"
+              className="inline-flex items-center gap-2 rounded-lg bg-gray-200 px-2.5 py-2 text-sm font-semibold text-gray-500 ring-indigo-300 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:hidden"
+              onClick={toggleMobileMenu} // Add click event handler
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" />
+              </svg>
+              Menu
+            </button>
 
-            Menu
-          </button>
+            {/* Mobile Menu */}
+            {isMobileMenuOpen && (
+              <div className="lg:hidden">
+                <ul className="mt-4 text-center">
+                  <li className="mb-3">
+                    <Link to="/" className="block text-lg font-semibold text-gray-600 hover:text-indigo-500 active:text-indigo-700">Home</Link>
+                  </li>
+                  <li className="mb-3">
+                    <Link to="/about" className="block text-lg font-semibold text-gray-600 hover:text-indigo-500 active:text-indigo-700">About</Link>
+                  </li>
+                  <li>
+                    <Link to="/contact" className="block text-lg font-semibold text-white bg-indigo-500 py-2 px-4 rounded-full hover:bg-indigo-600 active:bg-indigo-700 md:text-base">Contact</Link>
+                  </li>
+                </ul>
+              </div>
+            )}
         </header>
 
       
